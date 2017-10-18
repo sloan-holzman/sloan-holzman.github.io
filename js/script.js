a44b5b27e1f76df93f31355b357b29e527bebf7b
Sloan Holzman <sloan.holzman@gmail.com>
2017-10-17 22:20:45 -0400

    add jquery, scroll, but only works from top of page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("hello")
+
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+
+  var  position = $($(this).attr("href")).offset().top;
+
+  $("main, body, html").animate({
+		scrollTop: (position - 50)
+	}, 500, 'linear' );
+});
