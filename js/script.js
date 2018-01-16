75e1ebd3453b7a0845990c0424e7a7458f967047
Sloan Holzman <sloan.holzman@gmail.com>
2018-01-16 15:05:11 -0500

    update text, reorder projects

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// console.log("hello")
-//
-// $("a[href^='#']").click(function(e) {
-// 	e.preventDefault();
-//
-//   var  position = $($(this).attr("href")).offset().top;
-//
-//   $("main, body, html").animate({
-// 		scrollTop: (position - 50)
-// 	}, 500, 'linear' );
-// });
+window.sr = ScrollReveal();
+var projectContainer = document.getElementById('portfolio');
+sr.reveal('.project', { container: projectContainer });
